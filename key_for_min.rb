17e11e760516077e45cd59ed7f8cede41de8c88d
Dmitry Sychev <axiumone@gmail.com>
2020-06-08 20:34:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   value_arr = []
-  name_hash.each do |k,v|
-    value_arr << v
+  name_hash.each do |k1,v1|
+    name_hash.each do |k2,v2|
+      if v2 > v1
+        k
+      end
+    end
   end
-  low_v = value_arr.sort
-  ans_value = low_v[0]
 end
