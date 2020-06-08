d81e3606bf5a604da49452b90350317a88bef6d4
Dmitry Sychev <axiumone@gmail.com>
2020-06-08 20:26:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  value_arr = []
+  name_hash.each do |k,v|
+    value_arr << v
+  end
+  answer = value_arr.sort
+  answer[0]
 end
